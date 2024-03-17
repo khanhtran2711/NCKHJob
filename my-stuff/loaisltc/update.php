8547231da7bb71649557b0e8fd10353162524442
minhldnex <minh.ly@usnexgen.com>
2024-03-17 15:48:48 +0700

    in loaisltc

--- my-stuff/loaisltc/update.php
+++ my-stuff/loaisltc/update.php
@@ -16,7 +16,7 @@ $c = $_POST['thoigian_apdung'];
 $id = $_POST['ma_loaisl'];
 $d = $_POST['ma_loaict'];
 $sql = "update `$tablename` set ten_loaisl='".$a."', giatri_sl='".$b."',
-thoigian_apdung='".$c."',ma_loaict ='".$d."' where ma_loaisl=" . $id;
+thoigian_apdung='".$c."' where ma_loaisl=" . $id;
 // next line is for debugging, they appear in the php_error.log file
 // comment it out before putting into production
 error_log('sql = '.$sql);
